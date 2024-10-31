b65a1c25fca42c4668aa5721d7df3dc4b2f1b6b9
Satybaldy <satao2000326@gmail.com>
2024-10-31 19:56:54 +0500

    napsyrma

---
+++ hw1-week10/ss.js
@@ -0,0 +1,14 @@
+// var num = prompt('后面的答案' )
+ 
+
+// var kobei = num * num 
+
+// alert("жауабы " + kobei)
+
+var num = Number(prompt('width'))
+var num1 = Number(prompt('height'))
+
+var square = num * num1 
+var p = (num+num1)*2
+
+alert("natizhesy " + square + '\n' + "zhauaby" + p)
