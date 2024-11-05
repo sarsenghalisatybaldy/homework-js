25f6b7b4a02190289414c179410de550c6875dc9
Satybaldy <satao2000326@gmail.com>
2024-11-05 23:59:25 +0500

    hw2-week10

--- hw2-week10/index.js
+++ hw2-week10/index.js
@@ -7,3 +7,17 @@ if ((course === 1 || course === 2) && isAlmaty === "yes") {
 else {
   alert("你不符合");
 }
+
+
+
+let me = prompt("менің көнлім дегіні тауаласізба (иә or жок)")
+let travel = prompt("Japan or Korea?")
+let year = prompt("мен қайжіл (2000 or 1999?)")
+let birthday = prompt("менің туылқан кунім (3.24 3.30 3.26 3.27)")
+
+if (me === иә && travel === "Japan" && (year === 2000 || year === 0) && birthday === 3.26){
+    alert("қутыболсын");
+} 
+else{
+    alert("өкінышке орай")
+}
