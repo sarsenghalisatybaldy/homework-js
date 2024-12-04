52e46af5de6841a7eed2aa5bbceedaea1b5aa2a7
Satybaldy <satao2000326@gmail.com>
2024-12-05 00:22:21 +0500

    hw3-week14

---
+++ hw3-week14/index.js
@@ -0,0 +1,19 @@
+let students = [
+    { name: "Asan", math: 85, city: "Almaty" },
+    { name: "Bolat", math: 90 }
+  ];
+ 
+  let [student1] = students;
+  
+  console.log(students);
+
+
+
+
+
+let class1 = ["Student1", "Student2", "Student3"];
+let class2 = ["Student4", "Student5", "Student6"];
+
+let finalList = ["derektur", ...class1, ...class2, "mugulym"];
+
+console.log(finalList);
